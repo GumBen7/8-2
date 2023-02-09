55ead3d7193452e84dfe103c358dacce97b19ae1
Егор Сивцев <gumben7@gmail.com>
2023-02-09 12:01:20 +0700

    Initial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace EightTwo {
+    class Program {
+        static void Main(string[] args) {
+             var n = int.Parse(Console.ReadLine());
+             var sArr = Console.ReadLine().Split();
+             int[] a = new int[n];
+             for (var i = 0; i < n; ++i) {
+                a[i] = int.Parse(sArr[i]);
+             }
+        }
+    }
+}
